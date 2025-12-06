a4bd32873bac0058474e4412591f837a1956b010
LeoCampos6 <pontoz314@gmail.com>
2025-12-06 15:21:54 -0300

    CreditsCode.cs

--- CreditsCode.cs
+++ CreditsCode.cs
@@ -15,8 +15,12 @@ public class Creditos : MonoBehaviour
 
     startButton = root.Q<Button>("startButton");
     creditsButton = root.Q<Button>("creditsButton");
+    backButton = root.Q<Button>("backButton");
 
-    if (startButton != null)
+    //esse DisplayStyle serve para deixar o botão de voltar inativo no menu inicial
+    backButton.style.display = DisplayStyle.None;
+
+    if (creditsButton != null)
     {
       creditsButton.clicked += Credito;
     }
@@ -30,8 +34,41 @@ public class Creditos : MonoBehaviour
   {
     startButton.style.display = DisplayStyle.None;
     creditsButton.style.display = DisplayStyle.None;
+    backButton.style.display = DisplayStyle.Flex;
     Fundo.SetActive(false);
     //Deixa o GameObject dos creditos desativado
     Credit.SetActive(true);
+
+    OnBack();
+  }
+
+  // Código para o botão de voltar
+  void OnBack()
+  {
+    var uiDocument = GetComponent<UIDocument>();
+    var root = uiDocument.rootVisualElement;
+
+    startButton = root.Q<Button>("startButton");
+    creditsButton = root.Q<Button>("creditsButton");
+    backButton = root.Q<Button>("backButton");
+
+    if (backButton != null)
+    {
+      backButton.clicked += Back;
+    }
+    else
+    {
+      Debug.LogError("Botão não encontrado");
+    }
+  }
+  
+  void Back()
+  {
+    startButton.style.display = DisplayStyle.Flex;
+    creditsButton.style.display = DisplayStyle.Flex;
+    backButton.style.display = DisplayStyle.None;
+    Fundo.SetActive(true);
+    //Desativa o GameObject dos creditos novamente
+    Credit.SetActive(false);
   }
 }
