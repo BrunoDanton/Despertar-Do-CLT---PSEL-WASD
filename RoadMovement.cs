8263974996e11e7e6d3e44cdf9b2555cbffe5e43
BrunoDanton <danton.carneiro@dcomp.ufs.br>
2025-12-07 10:35:37 -0300

    Refactor RoadMovement for improved movement handling
    Refactored movement logic to use a switch statement and added a multiplicador for adjustable speed.

--- RoadMovement.cs
+++ RoadMovement.cs
@@ -2,27 +2,29 @@ using UnityEngine;
 
 public class RoadMovement : MonoBehaviour
 {
+    // VARIÁVEIS ESTÁTICAS GLOBAIS
     public static float globalVelocity = -10; 
-    public static int direction = 0;
+    public static int direction = 0; // 0: Z Positivo (Default) | 1: X Positivo | 2: Z Negativo | 3: X Negativo
+    public static float multiplicador = 1;
 
     private void Update()
     {
+        float movementAmount = globalVelocity * Time.deltaTime * multiplicador;
 
-        if (direction == 0)
+        switch (direction)
         {
-            transform.position += new Vector3(0, 0, globalVelocity * Time.deltaTime);
-        }
-        else if (direction == 1)
-        {
-            transform.position += new Vector3(globalVelocity * Time.deltaTime, 0, 0);
-        }
-        else if (direction == 2)
-        {
-            transform.position += new Vector3(0, 0, -globalVelocity * Time.deltaTime);
-        }
-        else
-        {
-            transform.position += new Vector3(-globalVelocity * Time.deltaTime, 0, 0);
+            case 0: // Movimento Padrão (Frente/Z positivo)
+                transform.position += new Vector3(0, 0, movementAmount);
+                break;
+            case 1: // Virar para X Positivo
+                transform.position += new Vector3(movementAmount, 0, 0);
+                break;
+            case 2: // Virar para Z Negativo (Volta, se globalVelocity for negativo)
+                transform.position += new Vector3(0, 0, -movementAmount);
+                break;
+            case 3: // Virar para X Negativo
+                transform.position += new Vector3(-movementAmount, 0, 0);
+                break;
         }
     }
 }
